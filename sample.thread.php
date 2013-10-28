a5a63984f8b5bf26b97d8744cf8995a6a0a66216
Dr. Jason KIM <jason.gcg@gmail.com>
2013-10-28 19:48:40 +0800

    Readme

--- sample.thread.php
+++ sample.thread.php
@@ -1,13 +1,4 @@
-PHP多线程服务和MapReduce能力库
-=========
-
-使用PHP的curl_multi方法来实现的多线程并发。
-----------
-###依赖性
-PHP curl
-###示例
-<?
-//sample.thread.php
+<?php
 include_once 'common.inc.php';
 
 $threads=new ThreadPool();
@@ -20,6 +11,5 @@ $result=$threads->execThreads();
 $t1_result=$result[$t1->getThreadID()];
 $t2_result=$result[$t2->getThreadID()];
 $t3_result=$result[$t3->getThreadID()];
-$t4_result=$result[$t4->getThreadID()]; 
+$t4_result=$result[$t4->getThreadID()];
 ?>
-test1.php到test4.php均会同时多线程执行，结果返回$result中，分别用其线程ID区分。
